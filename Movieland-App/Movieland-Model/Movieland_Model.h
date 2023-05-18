e0b086032345e5006ce570ddaef018557fc31e81
Massimiliano Faustini <>
2023-05-18 12:15:40 +0200

    added framework module for model

---
+++ Movieland-App/Movieland-Model/Movieland_Model.h
@@ -0,0 +1,18 @@
+//
+//  Movieland_Model.h
+//  Movieland-Model
+//
+//  Created by Massimiliano Faustini on 18/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Movieland_Model.
+FOUNDATION_EXPORT double Movieland_ModelVersionNumber;
+
+//! Project version string for Movieland_Model.
+FOUNDATION_EXPORT const unsigned char Movieland_ModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Movieland_Model/PublicHeader.h>
+
+
